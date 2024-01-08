c1e89955bfe8e58d086df39c4c39684f926b8fa4
chunchuAkhil <akhilchunchu52@gmail.com>
2024-01-08 20:46:35 +0530

    added login form submit

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Anchor,
   Button,
@@ -10,9 +12,21 @@ import {
   TextInput,
   Title,
 } from '@mantine/core';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 
 const Login: React.FC = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent): void => {
+    e.preventDefault();
+    if (email === 'admin1@test.com' && password === 'admin1') {
+      router.push('/');
+    }
+  };
+
   return (
     <Container size={420} my={40}>
       <Title ta="center">Welcome back!</Title>
@@ -24,24 +38,39 @@ const Login: React.FC = () => {
       </Text>
 
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="you@mantine.dev" required />
-        <PasswordInput
-          label="Password"
-          placeholder="Your password"
-          required
-          mt="md"
-        />
-        <Group justify="space-between" mt="lg">
-          <Checkbox label="Remember me" />
-          <Anchor component="button" size="sm">
-            Forgot password?
-          </Anchor>
-        </Group>
-        <Button fullWidth mt="xl">
-          Sign in
-        </Button>
+        <form onSubmit={handleSubmit}>
+          <TextInput
+            label="Email"
+            placeholder="you@mantine.dev"
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            required
+            mt="md"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <Group justify="space-between" mt="lg">
+            <Checkbox label="Remember me" />
+            <Anchor component="button" size="sm">
+              Forgot password?
+            </Anchor>
+          </Group>
+          <Button type="submit" fullWidth mt="xl">
+            Sign in
+          </Button>
+        </form>
       </Paper>
     </Container>
   );
 };
+
 export default Login;
